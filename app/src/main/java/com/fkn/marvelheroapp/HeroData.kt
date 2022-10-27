345e3436ad33ea0893719d04cb470fc0aee0c228
Конышев Виктор <konvit16@gmail.com>
2022-10-28 00:51:45 +0600

    Add files

---
+++ app/src/main/java/com/fkn/marvelheroapp/HeroData.kt
@@ -0,0 +1,11 @@
+package com.fkn.marvelheroapp
+
+import androidx.compose.ui.graphics.Color
+
+class HeroData(nameHero: String, descriptionHero: String, linkHero: String, imageHero: Int, colorHero: Color) {
+    var name = nameHero
+    var description = descriptionHero
+    var link = linkHero
+    var image = imageHero
+    var color = colorHero
+}
